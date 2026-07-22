30daf2de74222328591f33557f1314c2da90a154
Ythalo Saldanha <ythalorossy@gmail.com>
2026-07-22 18:00:07 -0400

    feat(web): generate catalog DTO types into contracts library

--- web/projects/contracts/src/lib/contracts.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-contracts',
-  imports: [],
-  template: ` <p>contracts works!</p> `,
-  styles: ``,
-})
-export class Contracts {}
